01e4017feba347a013ee4bd5d246f398888feafa
benkdrager <benkdrag@gmail.com>
2025-12-10 14:23:41 +0100

    Update

--- shell/bms_beacon.sh
+++ shell/bms_beacon.sh
@@ -7,24 +7,24 @@ source ${PERLBREW_ROOT}/etc/bashrc
 Date=$(date '+%Y-%m-%d')
 
 # tp
-#cp -f /home_ext/PK/siwallor/bms_beacon/template/beacon_tp.txt /home_ext/PK/siwallor/bms_beacon/dmp/ &&
-#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tp.txt &&
-#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tpv1 and pica.tbs=s and pica.tbs=m)' to Text --fields PPN --field_sep '||' --fix /home_ext/PK/siwallor/bms_beacon/fix/beacon_sru_map.fix >> /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tp.txt &&
+#cp -f $HOME/bms_beacon/template/beacon_tp.txt $HOME/bms_beacon/dmp/ &&
+#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" $HOME/bms_beacon/dmp/beacon_tp.txt &&
+#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tpv1 and pica.tbs=s and pica.tbs=m)' to Text --fields PPN --field_sep '||' --fix $HOME/bms_beacon/fix/beacon_sru_map.fix >> $HOME/bms_beacon/dmp/beacon_tp.txt &&
 # tb_kim
-#cp -f /home_ext/PK/siwallor/bms_beacon/template/beacon_tb.txt /home_ext/PK/siwallor/bms_beacon/dmp/ &&
-#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tb.txt &&
-#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tbv1 and pica.tbs=s and pica.tbs=m and pica.ent=kim)' to Text --fields PPN --field_sep '||' --fix /home_ext/PK/siwallor/bms_beacon/fix/beacon_sru_map.fix >> /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tb.txt &&
+#cp -f $HOME/bms_beacon/template/beacon_tb.txt $HOME/bms_beacon/dmp/ &&
+#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" $HOME/bms_beacon/dmp/beacon_tb.txt &&
+#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tbv1 and pica.tbs=s and pica.tbs=m and pica.ent=kim)' to Text --fields PPN --field_sep '||' --fix $HOME/bms_beacon/fix/beacon_sru_map.fix >> $HOME/bms_beacon/dmp/beacon_tb.txt &&
 # tu_wis
-cp -f /home_ext/PK/siwallor/bms_beacon/template/beacon_tuwis.txt /home_ext/PK/siwallor/bms_beacon/dmp/ &&
-sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwis.txt &&
-catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tuv1 and pica.ent=wis)' to Text --fields PPN --field_sep '||' --fix /home_ext/PK/siwallor/bms_beacon/fix/beacon_sru_map.fix >> /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwis.txt &&
+cp -f $HOME/bms_beacon/template/beacon_tuwis.txt $HOME/bms_beacon/dmp/ &&
+sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" $HOME/bms_beacon/dmp/beacon_tuwis.txt &&
+catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tuv1 and pica.ent=wis)' to Text --fields PPN --field_sep '||' --fix $HOME/bms_beacon/fix/beacon_sru_map.fix >> $HOME/bms_beacon/dmp/beacon_tuwis.txt &&
 # tu_wim
-#cp -f /home_ext/PK/siwallor/bms_beacon/template/beacon_tuwim.txt /home_ext/PK/siwallor/bms_beacon/dmp/ &&
-#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwim.txt &&
-#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tuv1 and pica.tbs=s and pica.tbs=m and pica.ent=wim)' to Text --fields PPN --field_sep '||' --fix /home_ext/PK/siwallor/bms_beacon/fix/beacon_sru_map.fix >> /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwim.txt &&
+#cp -f $HOME/bms_beacon/template/beacon_tuwim.txt $HOME/bms_beacon/dmp/ &&
+#sed -i "s/TIMESTAMP: [0-9]\+-[0-9]\+-[0-9]\+/TIMESTAMP: $Date/g" $HOME/bms_beacon/dmp/beacon_tuwim.txt &&
+#catmandu convert SRU --base 'http://sru.k10plus.de/bmsonline!rec=2' --recordSchema picaxml --parser picaxml --query '(pica.bbg=Tuv1 and pica.tbs=s and pica.tbs=m and pica.ent=wim)' to Text --fields PPN --field_sep '||' --fix $HOME/bms_beacon/fix/beacon_sru_map.fix >> $HOME/bms_beacon/dmp/beacon_tuwim.txt &&
 
-cd /home_ext/PK/siwallor/bms_beacon
+cd $HOME/bms_beacon
 
-git add /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tp.txt /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tb.txt /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwis.txt /home_ext/PK/siwallor/bms_beacon/dmp/beacon_tuwim.txt &&
+git add $HOME/bms_beacon/dmp/beacon_tp.txt $HOME/bms_beacon/dmp/beacon_tb.txt $HOME/bms_beacon/dmp/beacon_tuwis.txt $HOME/bms_beacon/dmp/beacon_tuwim.txt &&
 git commit -am "Update" &&
 git push -u origin
